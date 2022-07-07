0d23926145dd4b813be22145cb748abef926cb56
Cbillamil <108565366+Cbillamil@users.noreply.github.com>
2022-07-06 23:14:07 -0500

    Tema 9

---
+++ Personados.java
@@ -0,0 +1,46 @@
+public class Personados {
+
+    public static void main(String [] args) {
+        String nombre;
+        int telefono, edad;
+        
+        nombre = "Alex";
+        telefono = 25664254;
+        edad = 25;
+
+        System.out.println("El nombre es: " + nombre );
+        System.out.println("El Telefono es: " + telefono);
+        System.out.println("La edad es: " + edad);
+        
+
+        Cliente credito = new Cliente();
+        credito.setCredito("1000 USD");
+        System.out.println("El credito de la persona es: " + credito.getCredito());
+        Trabajador salario = new Trabajador();
+        salario.setSalario("5000 USD");
+        System.out.println("El salario de la persona es: " + salario.getSalario());
+    }
+}
+class Cliente extends Personados {
+    String credito ;
+
+    public String getCredito() {
+        return credito;
+    }
+
+    public void setCredito(String credito) {
+        this.credito = credito;
+    }
+    
+}
+class Trabajador extends Personados {
+    String salario;
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+} 
